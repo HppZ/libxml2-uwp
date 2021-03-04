8908f4786f63fbe23a65596fa00bfd630c702ec7
haopeng <haopeng@bilibili.com>
2021-03-04 19:26:35 +0800

    init commit

---
+++ App5/MainPage.xaml.cpp
@@ -0,0 +1,40 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+#include <libxml/xmlwriter.h>
+
+
+using namespace App5;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	Loaded += ref new RoutedEventHandler(this, &App5::MainPage::OnLoaded);
+}
+
+void MainPage::OnLoaded(Platform::Object^ obj, RoutedEventArgs^ args)
+{
+	// call any function from libxml2
+	xmlNewTextWriterFilename("test", 1);
+}
+
+
+
---
+++ App5/MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace App5
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+		void OnLoaded(Platform::Object^ obj, Windows::UI::Xaml::RoutedEventArgs^ args);
+	};
+}
